eba3ebb558d4e0d1b9e82007423bed25e33bbad9
EvanGudmestad <eagudmestad@ranken.edu>
2023-08-01 14:20:06 -0500

    Updated Bug List with a search interface

--- src/components/BugItem.jsx
+++ src/components/BugItem.jsx
@@ -3,14 +3,18 @@ import {Link} from 'react-router-dom';
 
 export function BugItem({bug,auth, index}) {
   return (
-   
+   <>
     <tr>
-        <th scope="row">{index+1}</th>
+        
+        <td>{index+1}</td>
         <td>{bug.title}</td>
         <td>{bug.classification}</td>
         <td>{moment(bug.createdOn).fromNow()}</td>
-        <td className="badge text-bg-info align-middle">{bug.closed.toString()}</td>
+        <td>{bug.closed.toString()}</td>
+        <td>{bug.assignedTo.fullName}</td>
+        <td>{bug.bugAuthor.fullName}</td>
         <td><Link to={`/bug/${bug._id}`} className="btn btn-sm btn-warning">Edit Bug</Link></td>
   </tr>
-  );
+  </>
+  )
 }
--- src/components/BugList.jsx
+++ src/components/BugList.jsx
@@ -3,11 +3,18 @@ import axios from 'axios';
 import {useState,useEffect} from 'react';
 import _ from 'lodash';
 import { BugItem } from './BugItem';
+import {FaSearch} from 'react-icons/fa'
 
 export function BugList({auth}) {
 
     const [bugs,setBugs] = useState(null);
-
+    const [sortValue, setSortValue] = useState('newest');
+    const [keywords, setKeywords] = useState('');
+    const [classificationFilter, setClassificationFilter] = useState('');
+    const [minAge, setMinAge] = useState('');
+    const [maxAge,setMaxAge] = useState('');
+    const [closed,setClosed] = useState(false);
+    
     useEffect(() =>{
        axios(`${process.env.REACT_APP_API_URL}/api/bugs/list`,{
         headers:{authorization: `Bearer ${auth?.token}`}
@@ -32,50 +39,72 @@ export function BugList({auth}) {
        });
     },[auth]);
 
+    const onSubmitSearch = async (evt) => {
+      evt.preventDefault();
+      try{
+        const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/bugs/list`,{
+          headers:{authorization: `Bearer ${auth?.token}`},
+          params:{sortBy:sortValue,keywords:keywords, classification:classificationFilter, minAge:minAge,maxAge:maxAge, closed:closed}
+        });
+        setBugs(res.data);
+      }catch(err){
+        console.log(err);
+      }
+
+    }
+
   return (
+    
     <div className="container" id="listContainer">
-      <div className="d-flex p-2">
+      <div className="p-2 row">
         <h3>Search Results</h3>
-        <div
-          className="btn-group ms-4"
-          role="group"
-          aria-label="Button group with nested dropdown"
-        >
-          <button type="button" className="btn btn-info">
-            Sort By
-          </button>
-          <div className="btn-group" role="group">
-            <button
-              id="btnGroupDrop3"
-              type="button"
-              className="btn btn-info dropdown-toggle"
-              data-bs-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            ></button>
-            <div className="dropdown-menu" aria-labelledby="btnGroupDrop3">
-              <a className="dropdown-item" href="/">
-                Newest
-              </a>
-              <a className="dropdown-item" href="/">
-                Oldest
-              </a>
-              <a className="dropdown-item" href="/">
-                Title
-              </a>
-              <a className="dropdown-item" href="/">
-                Classification
-              </a>
-              <a className="dropdown-item" href="/">
-                Assigned To
-              </a>
-              <a className="dropdown-item" href="/">
-                Bug Author
-              </a>
-            </div>
+        <div className='col'>
+         <label className='form-label' htmlFor='txtKeywords'>Keywords:</label>
+         <input type='text' className='form-control' value={keywords} onChange={(evt) => setKeywords(evt.target.value)} />
+        </div>
+        <div className='col'>
+          <label className='form-label' htmlFor='selSort'>Sort By</label>
+            <br />
+            <select className="form-select form-select-sm primary" aria-label="Default select example" id='selSort' value={sortValue} onChange={(evt) => setSortValue(evt.target.value)}>
+                  <option value="newest">Newest</option>
+                  <option value="oldest">Oldest</option>
+                  <option value="title">Title</option>
+                  <option value="classification">Classification</option>
+                  <option value="assignedTo">Assigned To</option>
+                  <option value="createdBy">Created By</option>
+              </select>
+        </div>
+        <div className='col'>
+          <label className='form-label' htmlFor='selClassification'>Filter By Classification</label>
+          <select className="form-select form-select-sm primary" aria-label="Default select example" id='selClassification' value={classificationFilter} onChange={(evt) => setClassificationFilter(evt.target.value)}> 
+                  <option value="">Select a Classification</option>
+                  <option value="approved">approved</option>
+                  <option value="unclassified">unclassified</option>
+                  <option value="duplicate">duplicate</option>
+                  <option value="unapproved">unapproved</option>
+          </select>
+        </div>
+        <div className='col'>
+          <label htmlFor="txtMinAge" className='form-label'>Minimum Age</label>
+          <input type="number" name="txtMinAge" id="txtMinAge" className='form-control' onChange={(evt) => setMinAge(evt.target.value)} />
+        </div>
+        <div className='col'>
+        <label htmlFor="txtMaxAge" className='form-label'>Maximum Age</label>
+          <input type="number" name="txtMaxAge" id="txtMaxAge" className='form-control' onChange={(evt) => setMaxAge(evt.target.value)} />
+        </div>
+        <div className='col form-check'>
+          <label htmlFor="chkClosed" className='form-check-label'>Closed:</label>
+          <input type="checkbox" name="chkClosed" id="chkClosed" className='form-check-input' onChange={(evt) => setClosed(evt.target.checked)} />
+        </div>
+        <div className='row'>
+          <div className='col-md-3'>
+            <label htmlFor='btnSubmit'>Search:</label>
+            <br />
+              <button id='btnSubmit' type='submit' className='btn btn-secondary' onClick={(evt) => onSubmitSearch(evt)}><FaSearch /></button>
           </div>
         </div>
       </div>
+      
 
       <table className="table table-striped">
         <thead>
@@ -85,12 +114,17 @@ export function BugList({auth}) {
             <th scope="col">Classification</th>
             <th scope="col">Created</th>
             <th scope="col">Closed</th>
+            <th scope="col">Assigned To</th>
+            <th scope="col">Reported By</th>
+            <th scope="col">Edit Bug</th>
           </tr>
         </thead>
         <tbody>
-         {_.map(bugs, (bug, index) => (
+         {bugs && _.map(bugs, (bug, index) => (
             <BugItem bug={bug} auth={auth} key={bug._id} index={index} />
          ))}
+
+         {/* {bugs.length === 0 && <tr><td>No Bugs match this criteria</td></tr>} */}
         </tbody>
       </table>
     </div>
