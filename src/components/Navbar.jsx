cc3e6afb08e236265c8fa67cf42b94b9f96889ab
EvanGudmestad <eagudmestad@ranken.edu>
2023-08-17 14:01:02 -0500

    Updated Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export function Navbar({auth, onLogout, showSuccess}){
     return(
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <NavLink className="navbar-brand" to="/">Best Bug Tracker 2023!</NavLink>
+          <NavLink className="navbar-brand" to="/">Gudmestad Bug Tracker 2023!</NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
